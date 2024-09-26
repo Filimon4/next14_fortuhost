072f7032be6c5bf8037a68ba358bb20bee2f81d2
FILIMON <f.rykov@bk.ru>
2024-09-27 01:26:53 +0300

    changes

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,16 @@
-import React, { useState } from 'react'
-
+'use client'
+import React from 'react'
+import { usePathname } from 'next/navigation'
 import styles from "./Dashboard.module.scss";
 import BoardTitle from '@/components/Dashboard/PreviewBoard';
 import BoardProject from '@/components/Dashboard/ProjectBoard';
 
 const Dashboard = () => {
+  const pathname = usePathname()
+  if (pathname) {
+    console.log(pathname)
+  }
+
   return (
     <section className={styles.dashboard_projects}>
       <BoardTitle />
