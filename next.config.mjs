4247df19c4e6a0d388d078581d630c350a3e190c
FILIMON <f.rykov@bk.ru>
2024-09-27 23:58:06 +0300

    chagne

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    images: {
+        domains: ['fortuhost.ru'],
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'fortuhost.ru',
+                pathname: '/icons/**', 
+            },
+        ],
+    },
     static: {
         directory: 'public',
         fallback: true,
