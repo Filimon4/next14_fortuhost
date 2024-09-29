19318944105195327e58f8dbd63cdb61c9d05f1a
FILIMON <f.rykov@bk.ru>
2024-09-29 19:22:00 +0300

    project page with searchParams

--- src/app/dashboard/project/page.tsx
+++ src/app/dashboard/project/page.tsx
@@ -1,11 +1,14 @@
+'use client'
 import React from 'react'
 
 import styles from './ProjectId.module.scss'
 import ProjectHeader from '@/components/Projects/ProjectHeader'
 import ProjectInstances from '@/components/Projects/ProjectInstances'
 import ProjectLogs from '@/shared/ui/Project/ProjectLogs'
+import { useSearchParams } from 'next/navigation'
 
 const Project = () => {
+  const params = useSearchParams()
   return (
     <>
       <ProjectLogs />
